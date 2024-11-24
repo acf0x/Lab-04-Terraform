37c1a7080d84aa7f41236111602e21789b018d18
acf0x <alvaro.cf@proton.me>
2024-11-24 18:50:59 +0100

    [!] Path to certificate and private key in root folder

--- locals.tf
+++ locals.tf
@@ -1,3 +1,6 @@
+/* Directorio donde estan almacenados el certificado y clave privada
+   path.cwd = raiz del proyecto */
+
 locals {
   certificado_path = "${path.cwd}/my-certificate.pem"
   privatekey_path  = "${path.cwd}/my-private-key.pem"
